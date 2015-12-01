7751a072f934102bc71dd92be454896dc1585441
avocadojesus <jgarbey@gmail.com>
2015-11-30 21:25:23 -0800

    adds uhhh... things/?

--- app/router/index.js
+++ app/router/index.js
@@ -1,5 +1,12 @@
+var path = require('path');
+
 module.exports = {
   run: function (app) {
     require('./routes/users')(app);
+
+    app.get('/', function(req, res) {
+      console.log(__dirname)
+      res.sendFile(path.join(__dirname, '../views/socket-test.html'));
+    })
   }
 };
--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 var express = require('express');
 var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
 var securityLayer = require('./app/security');
 var router = require('./app/router');
 var middleware = require('./app/middleware');
+
+io.on('connection', function(socket){
+  socket.on('down', function() {
+    console.log('down');
+  });
+
+  socket.on('up', function() {
+    console.log('up');
+  });
+
+  socket.on('left', function() {
+    console.log('left');
+  });
+
+  socket.on('right', function() {
+    console.log('right');
+  });
+});
 
 module.exports = {
   start: function(port) {
     middleware.run(app);
     securityLayer.run(app);
     router.run(app);
-    app.listen(port);
+    app.set('view engine', 'jade');
+    app.set('views', './app/views');
+    http.listen(port);
+
+    console.log('listening on ' + port)
   }
 }
